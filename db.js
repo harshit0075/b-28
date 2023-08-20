6f099c017668d06977e7e0d3f4a5e9f0d259ba81
masaiharshit <harshit007.ara@gmail.com>
2023-08-20 19:43:22 +0530

    env

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose")
+require("dotenv").config()
 
-const connection=mongoose.connect("mongodb://localhost:27017/google_notes")
+const connection=mongoose.connect(process.env.mongoURL)
 
 module.exports={
     connection
